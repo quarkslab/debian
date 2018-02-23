f65adf0e26df7dc05d28d4c897146ad4f7757d21
Émile Morel <emorel@quarkslab.com>
2018-02-23 16:29:54 +0100

    remove swapoff

--- script/whiteout.sh
+++ script/whiteout.sh
@@ -24,7 +24,7 @@ if [ "x${swapuuid}" != "x" ]; then
     # Whiteout the swap partition to reduce box size
     # Swap is disabled till reboot
     swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
-    /sbin/swapoff "${swappart}"
+    #/sbin/swapoff
     dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
     /sbin/mkswap -U "${swapuuid}" "${swappart}"
 fi
